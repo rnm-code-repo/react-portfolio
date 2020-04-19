12b94960535b5c85481d4d3c05835f932be38971
Rekha Ballur <rnmadalli@gmail.com>
2020-04-18 23:59:17 -0400

    Updates

---
+++ precache-manifest.efa62fb70ac06bee199630fec4bd0eb0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e330c543f24d025ac0663dc7417bd872",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9bd5f7d1d664d41a272f",
+    "url": "/react-portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "d35ce0d06fec417fd34e",
+    "url": "/react-portfolio/static/css/main.86ecc824.chunk.css"
+  },
+  {
+    "revision": "9bd5f7d1d664d41a272f",
+    "url": "/react-portfolio/static/js/2.6a760069.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.6a760069.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d35ce0d06fec417fd34e",
+    "url": "/react-portfolio/static/js/main.f4527b81.chunk.js"
+  },
+  {
+    "revision": "c01136dc6b7e393f307b",
+    "url": "/react-portfolio/static/js/runtime-main.d92c7dfa.js"
+  },
+  {
+    "revision": "23932c1cf4774260bbd2b095f3c8d6cd",
+    "url": "/react-portfolio/static/media/Cookout.23932c1c.png"
+  },
+  {
+    "revision": "b98940b1b630afc74ad785569b772707",
+    "url": "/react-portfolio/static/media/FinTrack.b98940b1.png"
+  },
+  {
+    "revision": "d03ce7be23ba8bde11f6719417bb7b54",
+    "url": "/react-portfolio/static/media/NoteTaker.d03ce7be.png"
+  },
+  {
+    "revision": "4c09de1e2a193320c5fd17899f181169",
+    "url": "/react-portfolio/static/media/Weather.4c09de1e.png"
+  },
+  {
+    "revision": "7aa29788d1ab10419f690030b7b5be17",
+    "url": "/react-portfolio/static/media/background.7aa29788.png"
+  },
+  {
+    "revision": "fb3938af902dd1fbb89297b1fb0ae33d",
+    "url": "/react-portfolio/static/media/burger.fb3938af.png"
+  },
+  {
+    "revision": "864132f161d2f79020830d0c61e5a079",
+    "url": "/react-portfolio/static/media/empDir.864132f1.png"
+  },
+  {
+    "revision": "3a1d0df542db2d425376e2fc46ed677d",
+    "url": "/react-portfolio/static/media/profile.3a1d0df5.jpg"
+  }
+]);
